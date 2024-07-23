a9b97885b625272308aff7be4be95eac3bab80c7
TechRam09 <nsshreeram@gmail.com>
2024-07-23 23:13:56 +0530

    Update views.py

--- program4/app4/views.py
+++ program4/app4/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template
 
-# Create your views here.
+def layout(request):
+    return render(request,'layout.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def aboutUs(request):
+    return render(request,'aboutUs.html')
+
+def contactUs(request):
+    return render(request,'contactUs.html')
